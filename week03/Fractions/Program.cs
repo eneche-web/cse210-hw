d676e03fe067949d07b7222b00f7aae6d0970dc0
eneche-web <enechegizo@gmail.com>
2026-07-20 17:59:19 -0700

    fraction.cs

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+
+        Fraction f2 = new Fraction(5);
+
+        Fraction f3 = new Fraction(3, 4);
+
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());
+
+        Console.WriteLine(f1.GetDecinalValue());
+
+        Console.WriteLine();
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine();
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine();
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+        Console.WriteLine();
+
+        f1.SetTop(6);
+        f1.SetBottom(7);
+
+        Console.WriteLine("After using setters");
+
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+
+        Console.WriteLine(f1.GetFraction());
+        Console.WriteLine(f1.GetDecimal());
+
     }
 }
